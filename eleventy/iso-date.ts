7a451f8841505e33826644635c8063a3cedb880f
Chris Krycho <hello@chriskrycho.com>
2020-12-20 20:24:17 -0700

    Delete all 11ty-isms

--- eleventy/iso-date.ts
+++
@@ -1,6 +0,0 @@
-import { fromDateOrString } from './date-time'
-
-const isoDate = (date: Date | string): string =>
-   fromDateOrString(date).toISO({ includeOffset: true })
-
-export default isoDate
--- eleventy/locale-date.ts
+++
@@ -1,7 +0,0 @@
-import { fromDateOrString } from './date-time'
-import { TZ_OPTIONS } from './archive-by-year'
-
-export const localeDate = (date: Date | string, format = 'DDD'): string =>
-   fromDateOrString(date).toFormat(format, TZ_OPTIONS)
-
-export default localeDate
